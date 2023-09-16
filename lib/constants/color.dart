06061536f6c470455fc66e54af1ba6f4b4e15966
dragonarm007 <s6230613021@phuket.psu.ac.th>
2023-09-17 01:15:46 +0700

    change variable name, separate colour class

--- lib/constants/color.dart
+++ lib/constants/color.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 
+class DarkThemeColour {
+  Color primaryColour = const Color(0xff413132);
+  Color accentColour = const Color(0xffFFD2BB);
+}
+
+class LightThemeColour {
+  Color primaryColour = const Color(0xffDF0054);
+  Color secondaryColour = const Color(0xffFF8B6A);
+}
+
 class Colour {
-  Color lightPrimaryColor = const Color(0xffDF0054);
-  Color darkPrimaryColor = const Color(0xff480032);
-  Color secondaryColor = const Color(0xffFF8B6A);
-  Color accentColor = const Color(0xffFFD2BB);
+  static DarkThemeColour get darkThemeColour => DarkThemeColour();
+  static LightThemeColour get lightThemeColour => LightThemeColour();
 }
--- lib/constants/theme.dart
+++ lib/constants/theme.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 import 'color.dart';
 
-Colour _colour = Colour();
-
 class ThemeClass {
   static ThemeData lightTheme = ThemeData(
     primaryColor: ThemeData.light().scaffoldBackgroundColor,
     colorScheme: const ColorScheme.light().copyWith(
-        primary: _colour.lightPrimaryColor, secondary: _colour.secondaryColor),
+      primary: Colour.lightThemeColour.primaryColour,
+      secondary: Colour.lightThemeColour.secondaryColour,
+    ),
   );
 
   static ThemeData darkTheme = ThemeData(
     primaryColor: ThemeData.dark().scaffoldBackgroundColor,
     colorScheme: const ColorScheme.dark().copyWith(
-      primary: _colour.darkPrimaryColor,
+      primary: Colour.darkThemeColour.primaryColour,
+      secondary: Colour.darkThemeColour.accentColour,
     ),
   );
 }
